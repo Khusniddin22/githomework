71ea47024d56fa9fc0aa0fd61b46dcea4ca40077
Khisniddin <you@example.com>
2025-06-09 00:57:20 +0300

    created function mask_account_card

--- src/widget.py
+++ src/widget.py
@@ -1,2 +1,26 @@
-def mask_account_card(account_card: str) -> str:
+def mask_account_card(type_and_account_card: str) -> str:
     """Функция принимает номер карты или счета и возвращает замаскированный номер"""
+    account_card = ''
+    mask_account = ''
+    type_name = ''
+    count_digit = 0
+    for symbol in type_and_account_card: #отбрасываем тип и оставляем только номер
+        if symbol.isdigit():
+            account_card += symbol
+        else:
+            type_name += symbol
+
+    if len(account_card) == 20: # проверка на номер счета
+        mask_account = "**" + account_card[-4:]
+    else: #иначе это номер карты
+        for i in range(len(account_card)):
+            count_digit += 1
+            if count_digit == 5:
+                mask_account += " "
+                count_digit = 1
+            if i >= 6 and i <= 11:
+                mask_account += "*"
+            else:
+                mask_account += account_card[i]
+
+    return type_name + mask_account
