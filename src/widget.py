5905fa8e2a6330625e5b7aff42f389aafb2008a6
Khisniddin <you@example.com>
2025-06-09 01:32:04 +0300

    code is formatted

--- src/widget.py
+++ src/widget.py
@@ -1,18 +1,18 @@
 def mask_account_card(type_and_account_card: str) -> str:
     """Функция принимает номер карты или счета и возвращает замаскированный номер"""
-    account_card = ''
-    mask_account = ''
-    type_name = ''
+    account_card = ""
+    mask_account = ""
+    type_name = ""
     count_digit = 0
-    for symbol in type_and_account_card: #отбрасываем тип и оставляем только номер
+    for symbol in type_and_account_card:  # отбрасываем тип и оставляем только номер
         if symbol.isdigit():
             account_card += symbol
         else:
             type_name += symbol
 
-    if len(account_card) == 20: # проверка на номер счета
+    if len(account_card) == 20:  # проверка на номер счета
         mask_account = "**" + account_card[-4:]
-    else: #иначе это номер карты
+    else:  # иначе это номер карты
         for i in range(len(account_card)):
             count_digit += 1
             if count_digit == 5:
@@ -25,8 +25,8 @@ def mask_account_card(type_and_account_card: str) -> str:
 
     return type_name + mask_account
 
-def get_date(long_date: str) -> str:
-    """Функция возвращает дату в формате 'ДД.ММ.ГГГГ' """
-    correct_date = long_date[8:10] + '.' + long_date[5:7] + '.' + long_date[:4]
-    return correct_date
 
+def get_date(long_date: str) -> str:
+    """Функция возвращает дату в формате 'ДД.ММ.ГГГГ'"""
+    correct_date = long_date[8:10] + "." + long_date[5:7] + "." + long_date[:4]
+    return correct_date
