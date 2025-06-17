69e7d3cdd4cb17bcdca2eed1c8193b3cb45e093b
Khisniddin <you@example.com>
2025-06-18 01:07:39 +0300

    code is designed

--- src/proccesing.py
+++ src/proccesing.py
@@ -1,7 +1,9 @@
-from src.widget import get_date
-from typing import List, Dict
-def filter_by_state(list_of_dict: List[Dict], state='EXECUTED')->List[Dict]:
-    """Функция возвращает отфильтрованный список словарей, у которых ключ state соответствует указанному значению"""
+from typing import Dict, List
+
+
+def filter_by_state(list_of_dict: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+    """Функция возвращает отфильтрованный список словарей,
+    у которых ключ state соответствует указанному значению"""
     filtered_list = []
     for dictionary in list_of_dict:
         for key in dictionary:
@@ -10,8 +12,7 @@ def filter_by_state(list_of_dict: List[Dict], state='EXECUTED')->List[Dict]:
     return filtered_list
 
 
-def sort_by_date(list_of_dict: List[Dict], desc=True)->List[Dict]:
+def sort_by_date(list_of_dict: List[Dict], desc: bool = True) -> List[Dict]:
     """Функция возвращает отсортированный список по заданной дате"""
-    sorted_list = sorted(list_of_dict, key=lambda x: x['date'], reverse=desc)
+    sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=desc)
     return sorted_list
-
