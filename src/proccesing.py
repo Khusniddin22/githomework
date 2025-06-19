4ba23e7d20afaecd9bc4c355663f038bbbb78d7d
Khisniddin <you@example.com>
2025-06-19 17:52:29 +0300

    rename desc and add readme.md and changed line-length

--- src/proccesing.py
+++ src/proccesing.py
@@ -1,18 +1,17 @@
 from typing import Dict, List
 
 
-def filter_by_state(list_of_dict: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(list_of_dicts: List[Dict], state: str = "EXECUTED") -> List[Dict]:
     """Функция возвращает отфильтрованный список словарей,
     у которых ключ state соответствует указанному значению"""
     filtered_list = []
-    for dictionary in list_of_dict:
-        for key in dictionary:
-            if dictionary[key] == state:
-                filtered_list.append(dictionary)
+    for dictionary in list_of_dicts:
+        if state in dictionary.values():
+            filtered_list.append(dictionary)
     return filtered_list
 
 
-def sort_by_date(list_of_dict: List[Dict], desc: bool = True) -> List[Dict]:
+def sort_by_date(list_of_dicts: List[Dict], decreasing: bool = True) -> List[Dict]:
     """Функция возвращает отсортированный список по заданной дате"""
-    sorted_list = sorted(list_of_dict, key=lambda x: x["date"], reverse=desc)
+    sorted_list = sorted(list_of_dicts, key=lambda x: x["date"], reverse=decreasing)
     return sorted_list
