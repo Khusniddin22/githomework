0bea2a043a00b9c578d0f534435c2ab5e391f749
Khisniddin <you@example.com>
2025-07-27 12:16:26 +0300

    has tested all functions and tests coverage

--- tests/conftest.py
+++ tests/conftest.py
@@ -30,3 +30,17 @@ def identical_dates():
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
         {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
     ]
+
+
+@pytest.fixture
+def filter_by_currency_usd():
+    return {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702",
+    }
+
