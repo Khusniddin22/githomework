812f807130f63f89e95a1c302606ed785ee14d86
Khisniddin <you@example.com>
2025-08-01 01:36:49 +0300

    changed decorator log add module test_decorators the function log has been tested

--- src/decorators.py
+++ src/decorators.py
@@ -2,11 +2,15 @@ from functools import wraps
 
 
 def log(filename=None):
+    """Функция, которая автоматически логирует начало и конец выполнения функции,
+    а также ее результаты или возникшие ошибки."""
     def my_decorator(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
+            """Обертка функции"""
             try:
                 result = func(*args, **kwargs)
+                # Если ф-ия выполнилась, то выводится 'ok'
                 log_message = f'{func.__name__} ok'
                 if filename == None:
                     print(log_message)
@@ -14,16 +18,9 @@ def log(filename=None):
                     with open(filename, 'a') as file_log:
                         file_log.write(log_message)
                 return result
-            except ValueError:
-                error_message = f'{func.__name__}: Неправильно ввели данные. Input: ({args})'
-                if filename == None:
-                    print(error_message)
-                else:
-                    with open(filename, 'a') as file_log:
-                        file_log.write(error_message)
-                return None
-            except ZeroDivisionError:
-                error_message = f'{func.__name__}: На ноль делить нельзя!. Input: ({args})'
+            except Exception as e:
+                # Вызывается исключение и тип ошибки с введенными аргументами
+                error_message = f'{func.__name__}: {type(e).__name__}. Input: ({args})'
                 if filename == None:
                     print(error_message)
                 else:
@@ -33,8 +30,3 @@ def log(filename=None):
         return wrapper
     return my_decorator
 
-@log()
-def my_function(x, y):
-    return x / y
-
-my_function(1, 0)
