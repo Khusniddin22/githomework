a5186003291142ffeb9961e41ba3c52462754e4a
Khisniddin <you@example.com>
2025-08-06 18:53:48 +0300

    and function convert_currency

--- src/utils.py
+++ src/utils.py
@@ -4,13 +4,14 @@ from idlelib.iomenu import encoding
 
 
 def financial_transactions(path_file: str) -> list:
-    with open(path_file, 'r', encoding='utf-8') as f_tr:
+    with open(path_file, "r", encoding="utf-8") as f_tr:
         if f_tr is None:
             return []
         else:
             data = json.load(f_tr)
             return data
 
-abs_path_file = r'C:\Users\79623\Desktop\home_work\githomework\data\operations.json'
+
+abs_path_file = r"C:\Users\79623\Desktop\home_work\githomework\data\operations.json"
 
 print(financial_transactions(abs_path_file))
