f6e1cbba1fe79a854f382c9bb342a5d89227de80
Khisniddin <you@example.com>
2025-09-16 00:28:48 +0300

    add log utils

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,14 @@
 import json
 import os
+import logging
+
+
+logger_utils = logging.getLogger('utils')
+logger_utils.setLevel(logging.DEBUG)
+file_handler = logging.FileHandler('logs/utils.log')
+file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+file_handler.setFormatter(file_formatter)
+logger_utils.addHandler(file_handler)
 
 
 def financial_transactions(path_file: str) -> list:
@@ -10,11 +19,17 @@ def financial_transactions(path_file: str) -> list:
     try:
         with open(path_file, "r", encoding="utf-8") as f_tr:
             if f_tr is None:
+                logger_utils.warning(f'Файл {path_file} не открыт. Возвращается пустой список."')
                 return []
             else:
                 data = json.load(f_tr)
+                logger_utils.info(f"Файл {path_file} успешно загружен.")
                 return data
     except FileNotFoundError:
+        logger_utils.error(f'Файл {path_file} не найден. Возвращается пустой список.')
+        return []
+    except json.JSONDecodeError:
+        logger_utils.error(f"Некорректный формат JSON в файле {path_file}. Возвращается пустой список.")
         return []
 
 
