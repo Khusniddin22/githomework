b44cfb9b5929c842f9ebe2a6b690f2c1846596fa
Khisniddin <you@example.com>
2025-10-27 19:44:12 +0300

    add functions reading_transactions_csv and reading_transactions_excel

---
+++ src/transact_pd.py
@@ -0,0 +1,47 @@
+import csv
+
+import pandas as pd
+import os
+
+
+path_csv = os.path.join('..', 'data', 'transactions.csv')
+path_excel = os.path.join('..', 'data', 'transactions_excel.xlsx')
+
+def reading_transactions_csv(file_path_csv: str):
+    '''Считывает транзакции из CSV файла и возвращает список словарей'''
+    try:
+        transaction_list_csv = []
+        with open(file_path_csv, 'r', encoding='utf-8') as f_csv:
+            if f_csv is None:
+                return []
+            else:
+                reader = csv.DictReader(f_csv, delimiter=';')
+                for row in reader:
+                    transaction_list_csv.append(row)
+                return transaction_list_csv
+    except FileNotFoundError:
+        return []
+
+#print(reading_transactions_csv(path_csv))
+
+
+def reading_transactions_excel(file_path_excel: str):
+    '''Считывает транзакции из EXCEL файла и возвращает список словарей'''
+    try:
+        transactions_list_excel = []
+        df_excel = pd.read_excel(file_path_excel)
+        for index, row in df_excel.iterrows():
+            dict_row = {'id': row['id'],
+                        'state':row['state'],
+                        'date': row['date'],
+                        'amount': row['amount'],
+                        'currency_name': row['currency_name'],
+                        'currency_code': row['currency_code'],
+                        'from': row['from'],
+                        'to': row['to'],
+                        'description': row['description']
+                        }
+            transactions_list_excel.append(dict_row)
+        return transactions_list_excel
+    except FileNotFoundError:
+        return []
