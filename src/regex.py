ebe10805ecbd55b67062630ba14a4a3f01e5c0cd
Khisniddin <you@example.com>
2025-11-08 22:56:14 +0300

    add function process_bank_operations

--- src/regex.py
+++ src/regex.py
@@ -1,28 +1,44 @@
 import re
+from collections import Counter, defaultdict
+
 
 list_transactions = [
                     {'id': '650703',
                      'state': 'EXECUTED',
-                      'date': '2023-09-05T11:30:32Z'
+                      'date': '2023-09-05T11:30:32Z',
+                     'description': 'Перевод организации'
                       },
                      {'id': '3598919',
                       'state': 'EXECUTED',
                       'date': '2020-12-06T23:00:58Z',
+                      'description': 'Открытие вклада'
                       },
                      {'id': '593027',
                       'state': 'CANCELED',
                       'date': '2023-07-22T05:02:01Z',
+                      'description': 'Открытие вклада'
+                      },
+                     {'id': '4699552',
+                      'state': 'EXECUTED',
+                      'date': '2022-03-23T08:29:37Z',
+                      'description': 'Перевод организации'
                       }
                      ]
 
 def process_bank_search(data: list[dict], search: str)->list[dict]:
     list_of_found_transactions = []
-    for tranaction in data:
-        for value in tranaction.values():
+    for transaction in data:
+        for value in transaction.values():
             pattern = re.findall(search, value)
             if pattern:
-                list_of_found_transactions.append(tranaction)
+                list_of_found_transactions.append(transaction)
 
     return list_of_found_transactions
 
-print(process_bank_search(list_transactions, 'EXECUTED'))
+
+def process_bank_operations(data: list[dict], categories:list)->dict:
+    list_categories = []
+    for transaction in data:
+        if transaction['description'] in categories:
+            list_categories.append(transaction['description'])
+    return Counter(list_categories)
